147567c05c6f1214ee0b190a9698c7f4a506c2f6
Naghmatarunum <tarunum.shaik@gmail.com>
2019-08-24 21:49:44 +0530

    commit

---
+++ src/org/test/cts/A.java
@@ -0,0 +1,24 @@
+package org.test.cts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+	// to set the property for drivers
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\NAZEEM\\practise1\\Seleniumfristday\\Drivers\\geckodriver.exe");
+		
+		WebDriver driver = new FirefoxDriver ();
+		driver.get("http://azhar-paperpresentation.blogspot.com/2010/04/palm-vein-technology.html");
+		
+		driver.quit();
+		
+		
+		
+	}
+}
